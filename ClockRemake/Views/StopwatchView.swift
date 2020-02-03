bff7a2b14c9bfc44fe25847c03fdfd2bd59526b4
Tristan Pudell-Spatscheck <TristanTheAppDev@gmail.com>
2020-02-03 10:51:31 -0500

    Made the Timer (countdown) actually be in the TimerView.swift file

--- ClockRemake/Views/StopwatchView.swift
+++ ClockRemake/Views/StopwatchView.swift
@@ -7,33 +7,9 @@
 //
 
 import SwiftUI
-
 struct StopwatchView: View {
-    //refreshes timer every 1 second + creates timer
-    @State var currentDate: Date = Date() //starting date, @state recreates interface whenever value is changed
-    let refDate: Date = Date(timeIntervalSinceNow: 10000)
-    var timer: Timer {
-       Timer.scheduledTimer(withTimeInterval: 1, repeats: true) {_ in
-            self.currentDate = Date()
-        }
-    }
-    //updates text on screen using given timer invervals
     var body: some View {
-        VStack {
-            Text(countdownString(from: refDate))
-            .font(.title) //sets the font to a "title" font
-        }
-        .onAppear(perform: { // called when text appears
-            _ = self.timer
-        })
-        
-    }
-    //makes a countdown string using a start and end date
-    func countdownString(from date: Date) -> String {
-        let calendar = Calendar(identifier: .gregorian)
-        let components = calendar.dateComponents([.day, .hour, .minute, .second], from: currentDate, to: date)
-        let resultString: String = ("Days: \(components.day ?? 00) \nHours: \(components.hour ?? 0) \nMinutes: \(components.minute ?? 0) \nSeconds: \(components.second ?? 0)")
-        return resultString
+        Text("Stopwatch != Timer")
     }
 }
 
--- ClockRemake/Views/TimerView.swift
+++ ClockRemake/Views/TimerView.swift
@@ -9,10 +9,33 @@
 import SwiftUI
 
 struct TimerView: View {
-    var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"0.0"/*@END_MENU_TOKEN@*/)
+    //refreshes timer every 1 second + creates timer
+        @State var currentDate: Date = Date() //starting date, @state recreates interface whenever value is changed
+        let refDate: Date = Date(timeIntervalSinceNow: 10000)
+        var timer: Timer {
+           Timer.scheduledTimer(withTimeInterval: 1, repeats: true) {_ in
+                self.currentDate = Date()
+            }
+        }
+        //updates text on screen using given timer invervals
+        var body: some View {
+            VStack {
+                Text(countdownString(from: refDate))
+                .font(.title) //sets the font to a "title" font
+            }
+            .onAppear(perform: { // called when text appears
+                _ = self.timer
+            })
+            
+        }
+        //makes a countdown string using a start and end date
+        func countdownString(from date: Date) -> String {
+            let calendar = Calendar(identifier: .gregorian)
+            let components = calendar.dateComponents([.day, .hour, .minute, .second], from: currentDate, to: date)
+            let resultString: String = ("Days: \(components.day ?? 00) \nHours: \(components.hour ?? 0) \nMinutes: \(components.minute ?? 0) \nSeconds: \(components.second ?? 0)")
+            return resultString
+        }
     }
-}
 
 struct TimerView_Previews: PreviewProvider {
     static var previews: some View {
