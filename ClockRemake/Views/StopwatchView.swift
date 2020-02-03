0789cb8a10b7bc1860c25206065330e31d7ec5be
Ethan Humphrey <ethantheinnovator@gmail.com>
2020-02-03 09:55:48 -0500

    Fixed Tristan's code kinda

--- ClockRemake/Views/StopwatchView.swift
+++ ClockRemake/Views/StopwatchView.swift
@@ -11,22 +11,25 @@ import SwiftUI
 struct StopwatchView: View {
     //refreshes timer every 1 second + creates timer
     @State var currentDate: Date = Date() //starting date, @state recreates interface whenever value is changed
-    let refDate: Date //reference date
+    let refDate: Date = Date() //reference date
     var timer: Timer {
-        Timer.scheduledTimer(withTimeInterval: 1, repeats: true) {_ in
+        return Timer.scheduledTimer(withTimeInterval: 1, repeats: true) {_ in
             self.currentDate = Date()
         }
     }
     //updates text on screen using given timer invervals
     var body: some View {
-        Text(cdString(from: refDate))
+        VStack {
+            Text(countdownString(from: refDate))
+            .font(.title) //sets the font to a "title" font
+        }
         .onAppear(perform: { // called when text appears
-            _ = self.timer //initializes timer
+            //initializes timer
         })
-        .font(.title) //sets the font to a "title" font
+        
     }
     //makes a countdown string using a start and end date
-    func cdString(from date: Date) -> String {
+    func countdownString(from date: Date) -> String {
         let calendar = Calendar(identifier: .gregorian)
         let components = calendar.dateComponents([.day, .hour, .minute, .second], from: currentDate, to: date)
         let resultString: String = ("Days: \(components.day ?? 00) \nHours: \(components.hour ?? 0) \nMinutes: \(components.minute ?? 0) \nSeconds: \(components.second ?? 0)")
@@ -34,8 +37,8 @@ struct StopwatchView: View {
     }
 }
 
-struct Stopwatch_Previews: PreviewProvider {
-    static var previews: some View {
-        StopwatchView(refDate: Date(timeInterval: 10000, since: Date())) //refDate testing value
-    }
-}
+//struct Stopwatch_Previews: PreviewProvider {
+//    static var previews: some View {
+//        StopwatchView(refDate: Date(timeInterval: 10000, since: Date())) //refDate testing value
+//    }
+//}
