15f0984d24c1ba5ad4d9def4ec02b3643d70d580
IbrahimSyedIS <ibrahim0m0syed@gmail.com>
2020-02-05 08:29:31 -0500

    Added rows to World Clock

--- ClockRemake/Views/WorldClockView.swift
+++ ClockRemake/Views/WorldClockView.swift
@@ -9,13 +9,57 @@
 import SwiftUI
 
 struct WorldClockView: View {
+    
+    let zones = [IbraTimeZone(zone: ""), IbraTimeZone(zone: ""), IbraTimeZone(zone: "")]
+    
     var body: some View {
-        Text("Ibrahim Needs to PUSH TO GIT")
+        NavigationView {
+            VStack {
+                List {
+                    ForEach(zones) { zone in
+                        IbraTimeZoneRow(timeZone: zone)
+                    }
+                }
+            }
+            .navigationBarTitle("World Clock")
+            .navigationBarItems(leading: Button(action: {
+                
+            }, label: {
+                Text("hi")
+            }), trailing: Button(action: {
+                
+            }, label: {
+                Text("hi2")
+            }))
+        }
     }
 }
 
 struct WorldClockView_Previews: PreviewProvider {
     static var previews: some View {
         WorldClockView()
+    }
+}
+
+struct IbraTimeZone: Identifiable {
+    var id = UUID()
+    var zone: String
+}
+
+struct IbraTimeZoneRow: View {
+    @State var timeZone: IbraTimeZone
+    @State private var showGreeting = true
+    
+    var body: some View {
+        HStack {
+            Text("Here is the zone \(timeZone.zone)")
+            Toggle(isOn: $showGreeting) {
+                Text("Meow")
+            }.padding()
+            
+            if showGreeting {
+                Text("Test")
+            }
+        }
     }
 }
